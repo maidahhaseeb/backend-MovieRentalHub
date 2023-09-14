2675eb2ec980583ce7eab8ed91fb6acdb18d83f2
maidah <maidahhaseeba@gmail.com>
2023-09-13 23:07:56 -0400

    hopefully added health check endpoint correctly!

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors'); //importing the cors middleware
 const app = express();
 const port = process.env.PORT || 3001;
 
+//use cors middleware to allow cross-origin requests from all origins
+app.use(cors());
+
 app.get('/health', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello from BackEnd!');
 });
 
 app.listen(port, () => {
